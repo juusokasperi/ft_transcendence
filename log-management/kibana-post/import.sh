482dcb5b7c811fc365739b024eb290a3228d148e
lemercie <lemercie@student.hive.fi>
2025-09-06 17:21:29 +0300

    only add data views if they dont already exist

--- log-management/kibana-post/import.sh
+++ log-management/kibana-post/import.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
+
+# non-zero exit will kill the whole script
 set -e
 
 echo "Waiting for Kibana to be ready..."
-until curl -s http://kibana:5601/api/status | grep -q '"level":"available"'; do
+until curl -s "${KIBANA_URL}/api/status" | grep -q '"level":"available"'; do
   sleep 5
 done
 
-echo "Importing data views..."
-curl -X POST "http://kibana:5601/api/saved_objects/_import" \
-  -u ${ELASTIC_USER}:${ELASTIC_PASSWORD} \
-  -H "kbn-xsrf: true" \
-  --form file=@/data/logstash.ndjson
+echo "Checking for existing data views..."
 
-echo "Data views imported"
+EXISTS=$(curl -s \
+	-u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" \
+	-H "kbn-xsrf: true" \
+	"${KIBANA_URL}/api/data_views" \
+	| grep -c "logstash" || true) # do not kill the script if false
+
+if [ "$EXISTS" -gt 0 ]; then
+  echo "Data view already exists, skipping import."
+else
+	echo "Importing data views..."
+	curl -X POST "${KIBANA_URL}/api/saved_objects/_import" \
+	  -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" \
+	  -H "kbn-xsrf: true" \
+	  --form file=@/data/logstash.ndjson
+
+	echo "Data views imported"
+fi
