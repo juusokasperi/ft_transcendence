fdf49ffda3706fcfbf7539efb9059648a2cdcf63
Rychkov Iurii <irychkov@student.hive.fi>
2025-09-09 15:03:58 +0300

    Fix logout route prefix in API registration

--- backend/index.ts
+++ backend/index.ts
@@ -26,6 +26,7 @@ import './types/types.ts';
 
 const app = fastify({
   logger: true,
+  // trustProxy: true,
   ajv: {
     customOptions: { allErrors: true, removeAdditional: true },
   },
@@ -67,7 +68,7 @@ await runMigrations();
 app.register(userRoutes, { prefix: '/api/users' });
 app.register(friendsRoutes, { prefix: '/api/friends' });
 app.register(loginRoutes, { prefix: '/api/login' });
-app.register(logoutRoutes, { prefix: 'api/logout' });
+app.register(logoutRoutes, { prefix: '/api/logout' });
 app.register(signupRoutes, { prefix: '/api/signup' });
 app.register(resetPasswordRoutes, { prefix: '/api/reset-password' });
 
