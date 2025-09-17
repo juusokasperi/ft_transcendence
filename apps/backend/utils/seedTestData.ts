67e4c6f6e282ecca8e2042cf13022a86fcf53bb8
juusokasperi <juusor@gmail.com>
2025-09-17 21:36:12 +0300

    Reverted seedTestData

--- apps/backend/utils/seedTestData.ts
+++ apps/backend/utils/seedTestData.ts
@@ -23,37 +23,34 @@ const createUser = async (username: string, email: string, password: string) => 
   return uuid;
 };
 
-// // Create a few test users
-// const uuidJoe = await createUser('Joe', 'joe@test.com', 'testPassword!1');
-// const uuidBob = await createUser('Bob', 'bob@test.com', 'testPassword!1');
-// const uuidWil = await createUser('Wil', 'wil@test.com', 'testPassword!1');
-// // Make Joe and Bob friends
-// addFriend(uuidJoe, uuidBob);
-// respondToFriendReq(uuidBob, uuidJoe, true);
-// // Make Joe and Wil friends
-// addFriend(uuidJoe, uuidWil);
-// respondToFriendReq(uuidWil, uuidJoe, true);
-// // Bob wants to befriend Wil, but Wil has not responded
-// addFriend(uuidBob, uuidWil);
-// // Create a few matches
-// addMatch(11, 5, uuidBob, uuidJoe, 30, -10);
-// addMatch(11, 0, uuidBob, uuidJoe, 10, -5);
-// addMatch(10, 12, uuidWil, uuidBob, -2, 8);
-// addMatch(1, 11, uuidJoe, uuidWil, 0, 10);
-// addMatch(8, 11, uuidJoe, uuidWil, 5, 10, 1, 'final');
-// // Change UserProfileSettings for Joe and Bob
-// updateUserSettings(uuidJoe, {
-//   paddle_color: '#FF0000',
-//   photo_sensitive_mode: 1,
-//   color_blind_mode: 1,
-// });
-// updateUserSettings(uuidBob, { paddle_color: '#BB00FF', color_blind_mode: 2 });
+// Create a few test users
+const uuidJoe = await createUser('Joe', 'joe@test.com', 'testPassword!1');
+const uuidBob = await createUser('Bob', 'bob@test.com', 'testPassword!1');
+const uuidWil = await createUser('Wil', 'wil@test.com', 'testPassword!1');
+// Make Joe and Bob friends
+addFriend(uuidJoe, uuidBob);
+respondToFriendReq(uuidBob, uuidJoe, true);
+// Make Joe and Wil friends
+addFriend(uuidJoe, uuidWil);
+respondToFriendReq(uuidWil, uuidJoe, true);
+// Bob wants to befriend Wil, but Wil has not responded
+addFriend(uuidBob, uuidWil);
+// Create a few matches
+addMatch(11, 5, uuidBob, uuidJoe, 30, -10);
+addMatch(11, 0, uuidBob, uuidJoe, 10, -5);
+addMatch(10, 12, uuidWil, uuidBob, -2, 8);
+addMatch(1, 11, uuidJoe, uuidWil, 0, 10);
+addMatch(8, 11, uuidJoe, uuidWil, 5, 10, 1, 'final');
+// Change UserProfileSettings for Joe and Bob
+updateUserSettings(uuidJoe, {
+  paddle_color: '#FF0000',
+  photo_sensitive_mode: 1,
+  color_blind_mode: 1,
+});
+updateUserSettings(uuidBob, { paddle_color: '#BB00FF', color_blind_mode: 2 });
 
-// // Move Joe's and Bob's last_seen to 10 minutes ago, so they appear offline
-// const tenMinutesAgo = new Date(Date.now() - 10 * 60 * 1000);
-// updateLastSeen(uuidJoe, tenMinutesAgo);
-// updateLastSeen(uuidBob, tenMinutesAgo);
+// Move Joe's and Bob's last_seen to 10 minutes ago, so they appear offline
+const tenMinutesAgo = new Date(Date.now() - 10 * 60 * 1000);
+updateLastSeen(uuidJoe, tenMinutesAgo);
+updateLastSeen(uuidBob, tenMinutesAgo);
 
-const uuidWil = getUserByUsername('Wil');
-const uuidBob = getUserByUsername('Bob');
-addMatch(10, 0, uuidWil!.uuid, uuidBob!.uuid, 5, 10, 5, 'final');
