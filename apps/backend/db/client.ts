581e464ed9dfedc31d2787a20f8de09c0e0d4747
To0nsa <nicolas.lovis@hotmail.fr>
2025-09-18 09:33:02 +0300

    fix: Misconfigured SQLite cache size,

--- apps/backend/db/client.ts
+++ apps/backend/db/client.ts
@@ -9,7 +9,7 @@ db.pragma('foreign_keys = ON');
 
 db.pragma('journal_mode = WAL'); // Write-Ahead Logging Mode, allows reads while writes happen
 db.pragma('synchronous = NORMAL'); // Doesn't wait for OS confirm about writes
-db.pragma('cache_size = 512000'); // Uses 512MB cache for memory instead of default 2MB
+db.pragma('cache_size =  -524288'); // Uses 512MB cache for memory instead of default 2MB
 db.pragma('temp_store = memory'); // Temporary tables/sorts happen in RAM instead of disk.
 
 export default db;
