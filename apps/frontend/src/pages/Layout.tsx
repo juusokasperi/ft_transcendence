d9727361a0b0b3ca5b34a13a3383f4519490d9b3
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-09-24 18:56:17 +0300

    Refactor Home and Layout components for improved structure and responsiveness; update game descriptions and visuals

--- apps/frontend/src/pages/Layout.tsx
+++ apps/frontend/src/pages/Layout.tsx
@@ -1,4 +1,3 @@
-import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 import { Outlet } from 'react-router-dom';
 import { useEffect } from 'react';
@@ -14,13 +13,10 @@ const Layout: React.FC = () => {
   }, [user, userReady, navigate]);
 
   return (
-    <div className="flex h-screen flex-col">
-      <Navbar />
-      <div className="flex h-full">
-        <Sidebar />
-        <div className="h-full flex-1 p-4 pt-10 md:px-10">
-          <Outlet />
-        </div>
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="h-full flex-1 p-4 pt-24 md:px-10">
+        <Outlet />
       </div>
     </div>
   );
