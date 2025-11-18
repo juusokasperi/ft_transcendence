8164b58a6d9901c794e43a37269a621a72a88ed6
juusokasperi <juusor@gmail.com>
2025-11-18 12:11:09 +0200

    Put alias back to tournament context (just to be sure)

--- apps/matchmaking/utils/queue.ts
+++ apps/matchmaking/utils/queue.ts
@@ -11,7 +11,7 @@ export interface TournamentMatchContext {
   tournamentId: number;
   tournamentMatchId: number;
   tournamentStage: 'semifinal' | 'final' | 'bronze';
-  participants?: Array<{ participantId: number; userUuid: string }>;
+  participants?: Array<{ participantId: number; userUuid: string; alias?: string }>;
 }
 
 const MMR_BUCKET_SIZE = 50;
